2285b2a46f7cefd61332df8e46e59a7d7dc02036
Bell <jacobbrister71@gmail.com>
2022-07-15 21:10:39 -0400

    added warning for repos with more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issuesContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +8,10 @@ var getRepoIssues = function(repo) {
         if(response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                if(response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -50,6 +55,18 @@ var displayIssues = function(issues) {
 
         issuesContainerEl.appendChild(issuesEl);
     }
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("4ian/GDevelop");
